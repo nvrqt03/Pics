3a12064e76c2d29953f82cd26fe601d100875247
nvrqt03 <ajktg21@gmail.com>
2019-04-24 06:14:16 -0500

    updated css grid to align pictures properly

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React from "react";
 
-class ImageCard extends React.Component{
-    render() {
-        const {description, urls} = this.props.image;
-        return (
-            <div>
-                <img 
-                alt={description}
-                src={urls.regular} 
-                />
-            </div>
-        )
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {spans: 0};
+    this.imageRef = React.createRef();
+  }
 
-    }
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+
+    //using the event listener because at the time chrome had not created the dom element by the time componentDidMount, so was reporting clientHeight of 0 - in the video
+
+    //console.log(this.imageRef.current.clientHeight);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({spans})
+  }
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
